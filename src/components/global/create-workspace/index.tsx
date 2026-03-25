2bc9225a6cfb4cfb37e11bacd797b637203d5f88
Ashuvis <ashutoshvishwakarma2478@gmail.com>
2026-03-25 21:05:48 +0530

    new commit

--- src/components/global/create-workspace/index.tsx
+++ src/components/global/create-workspace/index.tsx
@@ -1,8 +1,22 @@
 import React from 'react'
-
+import { useQueryData } from '@/hooks/usequery'
+import { getWorkspaces } from '@/actions/workspace'
 type Props = {}
 
 const CreateWorkspace = (props: Props) => {
+  const {data} = useQueryData(['user-workspaces'], getWorkspaces)
+
+  const {data: plan}= data as {
+    status: number,
+    data: {
+      subscription: {
+        plan: 'pro' | 'free'
+         }|null
+        }
+      }
+
+
+      
   return (
     <div>CreateWorkspace</div>
   )
